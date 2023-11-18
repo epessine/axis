5cb748e5817c507dfe0bbd2325cdee2ed472bb6b
Eduardo Pessine <eduardopessine@gmail.com>
2023-11-18 14:23:56 -0300

    refactor: set constructor on axis attribute

--- src/Attributes/Axis.php
+++ src/Attributes/Axis.php
@@ -7,6 +7,11 @@ use Livewire\Attributes\Computed;
 #[\Attribute]
 final class Axis extends Computed
 {
+    public function __construct()
+    {
+        parent::__construct(true);
+    }
+
     public function getChartId(): string
     {
         return $this->getComponent()->getId().'::'.$this->getName();

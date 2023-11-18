2111eb4d684c6f325cabb91278eb89145ed714ba
Eduardo Pessine <eduardopessine@gmail.com>
2023-11-18 14:30:30 -0300

    test: set test env key

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,17 @@
 
 namespace Tests;
 
+use Illuminate\Contracts\Config\Repository;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     protected $enablesPackageDiscoveries = true;
+
+    protected function defineEnvironment($app)
+    {
+        tap($app['config'], function (Repository $config) {
+            $config->set('key', str()->random(16));
+        });
+    }
 }

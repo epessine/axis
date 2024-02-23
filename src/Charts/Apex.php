9d66ebd6f5ab2af5507e74294a91f1e81a6befbe
Eduardo Pessine <eduardopessine@gmail.com>
2024-02-22 23:01:52 -0300

    feat: implement fluent api for apex charts

--- src/Charts/Apex.php
+++ src/Charts/Apex.php
@@ -2,6 +2,7 @@
 
 namespace Axis\Charts;
 
+use Axis\Enums\Type;
 use Axis\Interfaces\Javascriptable;
 use Axis\Interfaces\Renderable;
 use Axis\Traits\AsAxisChart;
@@ -33,5 +34,76 @@ final class Apex implements Htmlable, Javascriptable, Renderable, Serializable
         return <<<JS
             () => \$chart.updateOptions({$this->js($this->config)});
         JS;
+    }
+
+    public function title(string $title): static
+    {
+        data_set($this->config, 'title.text', $title);
+
+        return $this;
+    }
+
+    /**
+     * @param  iterable<string>  $labels
+     */
+    public function labels(iterable $labels): static
+    {
+        data_set($this->config, 'xaxis.categories', [...$labels]);
+
+        return $this;
+    }
+
+    public function type(Type $type): static
+    {
+        if ($type === Type::Bar) {
+            data_set($this->config, 'plotOptions.bar.horizontal', true);
+        }
+
+        $type = match ($type) {
+            Type::Bar => 'bar',
+            Type::Column => 'bar',
+            Type::Line => 'line',
+            Type::Pie => 'pie',
+            Type::Radar => 'radar',
+        };
+
+        data_set($this->config, 'chart.type', $type);
+
+        return $this;
+    }
+
+    /**
+     * @param  iterable<mixed, mixed>  $options
+     */
+    public function options(iterable $options, bool $overwrite = false): static
+    {
+        /** @var array<string, mixed> $currentOptions */
+        $currentOptions = $this->config ?? [];
+
+        $options = $overwrite
+            ? [...$options]
+            : array_replace_recursive($currentOptions, [...$options]);
+
+        $this->config = ['series' => $this->config['series'], ...$options];
+
+        return $this;
+    }
+
+    /**
+     * @param  iterable<int|iterable<int>>  $data
+     * @param  iterable<mixed, mixed>  $options
+     */
+    public function series(string $name, iterable $data, iterable $options = []): static
+    {
+        $series = ['data' => [...$data], 'name' => $name, ...$options];
+
+        /** @var array<array<string, mixed>> $currentSeries */
+        $currentSeries = $this->config['series'] ?? [];
+
+        $currentSeries[] = $series;
+
+        data_set($this->config, 'series', $currentSeries);
+
+        return $this;
     }
 }

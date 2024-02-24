7a758e70b14f315109dc415725a33dd6a013c42a
Eduardo Pessine <eduardopessine@gmail.com>
2024-02-24 14:15:55 -0300

    feat: add direct methods for chart type

--- src/Traits/IsFluent.php
+++ src/Traits/IsFluent.php
@@ -15,4 +15,29 @@ trait IsFluent
     abstract public function options(iterable $options, bool $overwrite = false): static;
 
     abstract public function series(string $name, iterable $data, iterable $options = []): static;
+
+    public function bar(): static
+    {
+        return $this->type(Type::Bar);
+    }
+
+    public function pie(): static
+    {
+        return $this->type(Type::Pie);
+    }
+
+    public function radar(): static
+    {
+        return $this->type(Type::Radar);
+    }
+
+    public function line(): static
+    {
+        return $this->type(Type::Line);
+    }
+
+    public function column(): static
+    {
+        return $this->type(Type::Column);
+    }
 }
